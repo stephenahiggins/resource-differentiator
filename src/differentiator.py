a1a32f08e05deaaedbca8fc2de5376276830de53
Stephen Higgins <stephenahiggins@icloud.com>
2024-04-03 12:51:58 +0100

    Experimenting with prompts

--- src/differentiator.py
+++ src/differentiator.py
@@ -1,17 +1,19 @@
 import os
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 from langchain.chains.llm import LLMChain
 import textstat
 from apikey import apikey
 
 # Config
-os.environ['OPENAI_API_KEY'] = apikey
-placeholder = open("src/storage/human_rights.txt", "r").read()
-llm = OpenAI(temperature=0.9, max_tokens=1000, model_name="gpt-3.5-turbo-instruct")
+OPEN_API_MODEL = "gpt-4-turbo-preview"
+# OPEN_API_MODEL = "gpt-3.5-turbo"
 MAX_ATTEMPTS = 3
 DEBUG_DISABLE_BRITISH_ENGLISH_CORRECTION = True
+os.environ['OPENAI_API_KEY'] = apikey
+placeholder = open("src/storage/human_rights.txt", "r").read()
+llm = ChatOpenAI(temperature=0.9, max_tokens=1000, model_name=OPEN_API_MODEL)
 
 READING_AGE_TO_FLESCH_KINCAID_GRADE = {
     '4': [1],
@@ -57,6 +59,7 @@ def calculate_fk_grade_of_text(text):
 
 def calculate_reading_age_of_text(text):
     fk_grade = calculate_fk_grade_of_text(text)
+    # Map the FK grade to a reading age
     return fk_grade_to_reading_age(fk_grade)
 
 def correct_british_english(document, llm):
@@ -72,40 +75,42 @@ def differentiate_and_correct_document(document, target_reading_age, llm):
     fk_values_from_age = READING_AGE_TO_FLESCH_KINCAID_GRADE.get(target_reading_age)
     target_fk_grade = fk_values_from_age[0] # Assume the lowest reading age is the least risky (if more than one age is provided)
     print(f"Target FK grade: {target_fk_grade}")
+    # Setup the closest match variables. These will be updated as we iterate through the attempts
+    closest_age_diff = float('inf')  # Initialize with a high difference
+    closest_text = ""
+    closest_attempt = 0  # Track the attempt number of the closest match
+    # Start the attempt loop
     for attempt in range(1, MAX_ATTEMPTS + 1):
         differentiation_template = PromptTemplate(
             input_variables=['target_fk_grade', 'document'],
-            template="Adapt and shorten this text to match a Flesch-Kincaid Grade level of {target_fk_grade}. Only output the text, no comments or explanations. Text: {document}"
+            template="Modify this text to match a Flesch-Kincaid grade of {target_fk_grade} or less. It must not exceed this Flesch-Kincaid grade. Only output the text, no comments or explanations. Text: {document}"
         )
-        differentiation_chain = LLMChain(llm=llm, prompt=differentiation_template, verbose=False, output_key='target_reading_age')
+        differentiation_chain = LLMChain(llm=llm, prompt=differentiation_template, verbose=True)
         
-        with st.spinner(f'Processing for age(s) {target_reading_age}, attempt {attempt}...'):
+        with st.spinner(f'Processing for age {target_reading_age}, attempt {attempt}...'):
             differentiation_run = differentiation_chain.run(target_fk_grade=str(target_fk_grade), document=document)
             current_text = differentiation_run  # Assume differentiation_run returns the text
             # Setup "closest match" variables
             calculated_reading_age = calculate_reading_age_of_text(current_text)
-            closest_age_diff = float('inf')  # Initialize with a high difference
-            closest_text = ""
-            closest_attempt = 0  # Track the attempt number of the closest match
             
             age_diff = abs(calculated_reading_age - int(target_reading_age))
             
             if age_diff == 0:
                 # If the age difference is 0, we have an exact match
-                st.success(f'Exact match found for target age {target_reading_age} in attempt {attempt}.')
+                st.success(f'Exact match found for target age {target_reading_age} in attempt #{attempt}.')
                 closest_text = current_text
                 break
             if age_diff < closest_age_diff:
                 closest_age_diff = age_diff
                 closest_text = current_text
                 closest_attempt = attempt
-            
 
             # Update the warning with the latest attempt's reading age
             st.warning(f'Attempt {attempt}: Recorded reading age: {calculated_reading_age}')
                 
     # Warn about which attempt was closest to the target age
-    st.info(f'The closest match to the target age was from attempt #{closest_attempt}.')
+    if closest_attempt > 0:
+        st.info(f'The closest match to the target age was from attempt #{closest_attempt}.')
     
     # Correct the closest matching text for British English
     if DEBUG_DISABLE_BRITISH_ENGLISH_CORRECTION:
@@ -115,8 +120,7 @@ def differentiate_and_correct_document(document, target_reading_age, llm):
     return corrected_text
 
 def main():
-    st.title("Learning Material Differentiation")
-    st.markdown("""# Test Differentiator By Reading Age Test
+    st.markdown("""# Differentiator By Reading Age Test
 * This demo takes some sample text (by default a section from the [Hodder OCR Geography A Level textbook on human rights](https://www.hoddereducation.com/media/resources/he/Geography/A-level/9781471858703/OCR-A-level-Geography-sample-chapter.pdf).
 * It then uses Chat GPT to attempt to differentiate the work for the selected reading ages. 
 * The content returned from ChatGPT is then checked against the Flesch Kincaid grade.
@@ -124,7 +128,7 @@ def main():
 * If the returned text doesn't match the absolute value of the target reading age, the request is re-run. 
     * This happens 3 times
     * If no exact match is found, then the best of 3 is taken
-* Finally, the "best of 3" result is sent to Chat GPT to check for British English spelling, punctuation and grammar. """)
+* Finally, there's an option to send the "best of 3" result is sent to Chat GPT to check for British English spelling, punctuation and grammar. """)
     content = st.text_area("Enter your content", value=placeholder, height=400)
     selected_ages = st.multiselect("Select target reading ages:", READING_AGE_TO_FLESCH_KINCAID_GRADE.keys(), default=['6'])
 
